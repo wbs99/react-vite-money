443168b5e6bc954d32fba7894628ea749d5ea93d
dingtalk_xsgrpl <1134954328@qq.com>
2022-10-28 16:46:41 +0800

    完成 itemlist 时间相关

--- src/components/Items/ItemList.tsx
+++ src/components/Items/ItemList.tsx
@@ -1,7 +1,9 @@
-import { useEffect, useState } from "react"
-import { Overlay } from "react-vant"
+import { useState } from "react"
+import { DatetimePicker, Divider, Overlay, Popup } from "react-vant"
 import styled from "styled-components"
 import { Time } from "../../shared/time"
+import { Start } from "../../views/Start"
+import { Form, FormItem } from "../Form"
 import { Icon } from "../Icon"
 import { MainPage } from "../MainPage"
 import { Tab, Tabs } from "../Tabs"
@@ -12,11 +14,13 @@ type Props = {
 
 export const ItemList: React.FC<Props> = () => {
   const [selected, setSelected] = useState('本月')
-  const onTabChange = (name: string) => { setSelected(name) }
-
+  const onTabChange = (name: string) => {
+    name === '自定义时间' && setVisible(true)
+    setSelected(name)
+  }
   const [customTime, setCustomTime] = useState({
-    start: new Time(),
-    end: new Time()
+    start: new Time().format(),
+    end: new Time().format()
   })
   const time = new Time()
   const timeList = [
@@ -34,8 +38,21 @@ export const ItemList: React.FC<Props> = () => {
     }
   ]
 
-  const [visible, setVisible] = useState(true)
+  const [visible, setVisible] = useState(false)
 
+  const onSubmitCustomTime = (e: React.FormEvent) => {
+    e.preventDefault()
+  }
+  const onStartTimeChange = (start: string) => {
+    setCustomTime({ ...customTime, start })
+  }
+  const [popupVisible, setPopupVisible] = useState(false)
+  const onTimeConfirm = () => {
+    setPopupVisible(false)
+  }
+
+  const onCancel = () => { setVisible(false) }
+  const onConfirm = () => { setVisible(false) }
 
   return (
     <div>
@@ -73,19 +90,69 @@ export const ItemList: React.FC<Props> = () => {
                   <header>
                     请选择时间
                   </header>
-                  <main>
-                    <form>
-                      <div>111
-
-                      </div>
-                      <div>222
-
-                      </div>
-                    </form>
-                  </main>
+                  <SelectTimeWrapper>
+                    <Form onSubmit={onSubmitCustomTime}>
+                      <FormItem label="开始时间"
+                        content={
+                          <>
+                            <input value={customTime.start} readOnly={true} onClick={() => {
+                              setPopupVisible(true)
+                            }} className='formItem input' />
+                            <Popup
+                              position='bottom'
+                              visible={popupVisible}
+                              onClose={() => { setPopupVisible(false) }}
+                            >
+                              <DatetimePicker
+                                title='选择年月日'
+                                type='date'
+                                minDate={new Date(2020, 0, 1)}
+                                maxDate={new Date(2025, 10, 1)}
+                                value={customTime.start}
+                                onChange={onStartTimeChange}
+                                onCancel={() => { setPopupVisible(false) }}
+                                onConfirm={onTimeConfirm}
+                              />
+                            </Popup>
+                          </>
+                        }
+                      />
+                      <FormItem label="结束时间"
+                        content={
+                          <>
+                            <input value={customTime.end} readOnly={true}
+                              onClick={() => { setPopupVisible(true) }} className='formItem input' />
+                            <Popup
+                              position='bottom'
+                              visible={popupVisible}
+                              onClose={() => { setPopupVisible(false) }}
+                            >
+                              <DatetimePicker
+                                title='选择年月日'
+                                type='date'
+                                minDate={new Date(2020, 0, 1)}
+                                maxDate={new Date(2025, 10, 1)}
+                                value={customTime.end}
+                                onChange={onStartTimeChange}
+                                onCancel={() => { setPopupVisible(false) }}
+                                onConfirm={onTimeConfirm}
+                              />
+                            </Popup>
+                          </>
+                        }
+                      />
+                      <FormItem
+                        content={
+                          <div className="actions">
+                            <button type="button" onClick={onCancel}>取消</button>
+                            <button type="submit" onClick={onConfirm}>确认</button>
+                          </div>
+                        }
+                      />
+                    </Form>
+                  </SelectTimeWrapper>
                 </div>
               </Overlay>
-
             </OverlayWrapper>
 
           </>
@@ -112,6 +179,50 @@ const OverlayWrapper = styled.div`
     }
     > main{
     }
+  }
+}
+`
+
+const SelectTimeWrapper = styled.main`
+.actions{
+   text-align: right;
+  button {
+    border: none;
+    background: transparent;
+    padding: 8px 16px;
+    & + & {
+      margin-left: 16px;
+    }
+  }
+
+}
+.formItem {
+  min-height: var(--input-min-height);
+  max-width: 100%;
+  &.input {
+    flex-grow: 1;
+    border: 1px solid var(--input-border-color);
+    border-radius: var(--input-radius);
+    font-size: 18px;
+    font-family: inherit;
+    padding-left: 16px;
+    padding-right: 16px;
+    box-shadow: inset 0 0 3px var(--input-shadow);
+    &.error {
+      border-color: var(--error-color);
+    }
+  }
+  &_value {
+    display: flex;
+    margin-top: 4px;
+    > * {
+      flex-grow: 1;
+    }
+  }
+  &_errorHint {
+    margin-top: 4px;
+    color: var(--error-color);
+    font-size: 12px;
   }
 }
 `
